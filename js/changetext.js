e6544818ad4ed6196b6c309382d63c2c05b6a8f8
ExEr7um <exer7um@gmail.com>
2018-06-20 23:59:54 +0300

    Файл скрипта смены текста

---
+++ js/changetext.js
@@ -0,0 +1,5 @@
+function changeText2(){
+ var userInput = document.getElementById('userInput').value;
+ document.getElementById('boldStuff2').innerHTML = userInput;
+  userInput = document.getElementById('userInput').value="";
+}
