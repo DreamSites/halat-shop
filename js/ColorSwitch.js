9c0c789fdeffa1cd75f9431eb1ebe4f8eac8d050
DanilapRock <39446103+DanilapRock@users.noreply.github.com>
2018-06-22 01:06:32 +0300

    Смена цвета по нажатию на кнопку

---
+++ js/ColorSwitch.js
@@ -0,0 +1,7 @@
+function change_color(color){
+      if (event.button class=="b1") document.getElementById('signInput').style.color='blue';
+        document.getElementById('t').style.background='green';
+        document.getElementById('t').style.background='blue';
+        document.getElementById('t').style.background='green';
+        document.getElementById('t').style.background='blue';
+   }
