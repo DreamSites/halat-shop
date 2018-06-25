c3aa7ccd13ee5c4a40580930b41b44c87b2b43c9
DanilapRock <39446103+DanilapRock@users.noreply.github.com>
2018-06-25 23:13:57 +0300

    Загрузка картинок(Короны итп) и выбор шрифта

---
+++ js/fontselection.js
@@ -0,0 +1,5 @@
+  function fontchange() {
+    $(document).ready(function(){
+    document.getElementById("signInput").style.fontFamily = "Arial";  ;
+});
+}
--- js/loadimg.js
+++ js/loadimg.js
@@ -1,6 +1,55 @@
 function loadimg() {
   $(document).ready(function(){
-    var img=document.getElementById("img");
-      img.src="img/img1.jpg";
+    var
+     img=document.getElementById("img");
+     x=document.getElementById("img1");
+     x1=document.getElementById("img2");
+     x2=document.getElementById("img3");
+      img.src="img/img.png";
+      x.src="";
+      x1.src="";
+      x2.src="";
+
+});
+}
+function loadimg1() {
+  $(document).ready(function(){
+    var
+      img1=document.getElementById("img2");
+      x=document.getElementById("img");
+      x1=document.getElementById("img1");
+      x2=document.getElementById("img3");
+       img1.src="img/img2.png";
+       x.src="";
+       x1.src="";
+       x2.src="";
+
+
+});
+}
+function loadimg2() {
+  $(document).ready(function(){
+    var
+      img2=document.getElementById("img3");
+      x=document.getElementById("img1");
+      x1=document.getElementById("img");
+      x2=document.getElementById("img2");
+       img2.src="img/img3.png";
+       x.src="";
+       x1.src="";
+       x2.src="";
+});
+}
+function loadimg3() {
+  $(document).ready(function(){
+    var
+      img3=document.getElementById("img1");
+      x=document.getElementById("img3");
+      x1=document.getElementById("img2");
+      x2=document.getElementById("img");
+       img3.src="img/img1.png";
+       x.src="";
+       x1.src="";
+       x2.src="";
 });
 }
