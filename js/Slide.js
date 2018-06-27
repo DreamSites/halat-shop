498767fc46f40ed632f795d74baa57e4d2d41f0b
DanilapRock <39446103+DanilapRock@users.noreply.github.com>
2018-06-27 13:56:43 +0300

    Слайдер картинок на минималках

---
+++ js/Slide.js
@@ -0,0 +1,68 @@
+function Slide1() {
+  $(document).ready(function(){
+    var
+     img=document.getElementById("img");
+     x=document.getElementById("img1");
+     x1=document.getElementById("img2");
+     x2=document.getElementById("img3");
+      img.src="img/3.jpg";
+      x.src="";
+      x1.src="";
+      x2.src="";
+
+});
+}
+function Slide2() {
+  $(document).ready(function(){
+    var
+      img1=document.getElementById("img2");
+      x=document.getElementById("img");
+      x1=document.getElementById("img1");
+      x2=document.getElementById("img3");
+       img1.src="img/4.jpg";
+       x.src="";
+       x1.src="";
+       x2.src="";
+
+
+});
+}
+function Slide3() {
+  $(document).ready(function(){
+    var
+      img2=document.getElementById("img3");
+      x=document.getElementById("img1");
+      x1=document.getElementById("img");
+      x2=document.getElementById("img2");
+       img2.src="img/5.jpg";
+       x.src="";
+       x1.src="";
+       x2.src="";
+});
+}
+function Slide4() {
+  $(document).ready(function(){
+    var
+      img3=document.getElementById("img1");
+      x=document.getElementById("img3");
+      x1=document.getElementById("img2");
+      x2=document.getElementById("img");
+       img3.src="img/6.jpg";
+       x.src="";
+       x1.src="";
+       x2.src="";
+});
+}
+function Slide5() {
+  $(document).ready(function(){
+    var
+      img3=document.getElementById("img1");
+      x=document.getElementById("img3");
+      x1=document.getElementById("img2");
+      x2=document.getElementById("img");
+       img3.src="img/11.jpg";
+       x.src="";
+       x1.src="";
+       x2.src="";
+});
+}
