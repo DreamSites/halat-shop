69fe895b6d53a2f057fd91c1b93cc55c9f87df2c
ExEr7um <exer7um@gmail.com>
2018-07-26 12:07:00 +0300

    Автоматическое переключение навигации

--- js/navigation.js
+++ js/navigation.js
@@ -1,8 +1,27 @@
-$(document).ready(function() {
-  $('#navitem-1').click(function() {
+function navigation() {
+  var heroSliderOffset = $($('.heroSlider')).offset().top - 85;
+  var ourWorkOffset = $($('.ourWork')).offset().top - 85;
+  var constructorHeaderOffset = $($('#constructorHeader')).offset().top - 85;
+  var posTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+
+  if (posTop <= heroSliderOffset) {
     document.getElementById('navitem-1').className = 'navitem nav-active';
     document.getElementById('navitem-2').className = 'navitem';
     document.getElementById('navitem-3').className = 'navitem';
+  }
+  if (posTop >= ourWorkOffset && posTop <= constructorHeaderOffset) {
+    document.getElementById('navitem-1').className = 'navitem';
+    document.getElementById('navitem-2').className = 'navitem nav-active';
+    document.getElementById('navitem-3').className = 'navitem';
+  }
+  if (posTop >= constructorHeaderOffset) {
+    document.getElementById('navitem-1').className = 'navitem';
+    document.getElementById('navitem-2').className = 'navitem';
+    document.getElementById('navitem-3').className = 'navitem nav-active';
+  }
+}
+$(document).ready(function() {
+  $('#navitem-1').click(function() {
     var scroll_el = $('body');
     if ($(scroll_el).length != 0) {
       $('html, body').animate({
@@ -12,9 +31,6 @@ $(document).ready(function() {
     return false;
   });
   $('#navitem-2').click(function() {
-    document.getElementById('navitem-1').className = 'navitem';
-    document.getElementById('navitem-2').className = 'navitem nav-active';
-    document.getElementById('navitem-3').className = 'navitem';
     var scroll_el = $('.ourWork');
     if ($(scroll_el).length != 0) {
       $('html, body').animate({
@@ -24,9 +40,6 @@ $(document).ready(function() {
     return false;
   });
   $('#navitem-3').click(function() {
-    document.getElementById('navitem-1').className = 'navitem';
-    document.getElementById('navitem-2').className = 'navitem';
-    document.getElementById('navitem-3').className = 'navitem nav-active';
     var scroll_el = $('#constructorHeader');
     if ($(scroll_el).length != 0) {
       $('html, body').animate({
