c394ee68aeb27744c680e25ddcf3abef6d1563fc
ExEr7um <exer7um@gmail.com>
2018-08-05 13:31:26 +0300

    Доделан скрипт поля "Шрифт"

--- js/constructor.js
+++ js/constructor.js
@@ -19,79 +19,85 @@ $(document).ready(function() {
   $('#FP-1').click(function() {
     $(this).addClass('FP-Active');
     $(this).css('border-radius', '3px 3px 0 0');
-    $('#FP-2').attr('class','FP-Item');
-    $('#FP-3').attr('class','FP-Item');
-    $('#FP-4').attr('class','FP-Item');
-    $('#FP-5').attr('class','FP-Item');
-    $('#FP-6').attr('class','FP-Item');
+    $('#FP-2').attr('class', 'FP-Item');
+    $('#FP-3').attr('class', 'FP-Item');
+    $('#FP-4').attr('class', 'FP-Item');
+    $('#FP-5').attr('class', 'FP-Item');
+    $('#FP-6').attr('class', 'FP-Item');
     $('#FP-6').css('border-radius', '0 0 5px 5px');
     $('#sign').css('font-family', 'Academia');
     $('#FP-Placeholder').css('color', 'black');
+    $('#FP-Placeholder').html('Academia <img src = "img/expandArrow.svg" alt = "" style = "float: right; margin-right: 15px;">');
     $('.fontPicker').css('border', '1px solid #1EA896');
   })
   $('#FP-2').click(function() {
-    $('#FP-1').attr('class','FP-Item');
+    $('#FP-1').attr('class', 'FP-Item');
     $('#FP-1').css('border-radius', '5px 5px 0 0');
     $(this).addClass('FP-Active');
-    $('#FP-3').attr('class','FP-Item');
-    $('#FP-4').attr('class','FP-Item');
-    $('#FP-5').attr('class','FP-Item');
-    $('#FP-6').attr('class','FP-Item');
+    $('#FP-3').attr('class', 'FP-Item');
+    $('#FP-4').attr('class', 'FP-Item');
+    $('#FP-5').attr('class', 'FP-Item');
+    $('#FP-6').attr('class', 'FP-Item');
     $('#FP-6').css('border-radius', '0 0 5px 5px');
     $('#sign').css('font-family', 'Campus');
     $('#FP-Placeholder').css('color', 'black');
+    $('#FP-Placeholder').html('Campus <img src = "img/expandArrow.svg" alt = "" style = "float: right; margin-right: 15px;">');
     $('.fontPicker').css('border', '1px solid #1EA896');
   })
   $('#FP-3').click(function() {
-    $('#FP-1').attr('class','FP-Item');
+    $('#FP-1').attr('class', 'FP-Item');
     $('#FP-1').css('border-radius', '5px 5px 0 0');
-    $('#FP-2').attr('class','FP-Item');
+    $('#FP-2').attr('class', 'FP-Item');
     $(this).addClass('FP-Active');
-    $('#FP-4').attr('class','FP-Item');
-    $('#FP-5').attr('class','FP-Item');
-    $('#FP-6').attr('class','FP-Item');
+    $('#FP-4').attr('class', 'FP-Item');
+    $('#FP-5').attr('class', 'FP-Item');
+    $('#FP-6').attr('class', 'FP-Item');
     $('#FP-6').css('border-radius', '0 0 5px 5px');
     $('#sign').css('font-family', 'CyrillicOld');
     $('#FP-Placeholder').css('color', 'black');
+    $('#FP-Placeholder').html('CyrillicOld <img src = "img/expandArrow.svg" alt = "" style = "float: right; margin-right: 15px;">');
     $('.fontPicker').css('border', '1px solid #1EA896');
   })
   $('#FP-4').click(function() {
-    $('#FP-1').attr('class','FP-Item');
+    $('#FP-1').attr('class', 'FP-Item');
     $('#FP-1').css('border-radius', '5px 5px 0 0');
-    $('#FP-2').attr('class','FP-Item');
-    $('#FP-3').attr('class','FP-Item');
+    $('#FP-2').attr('class', 'FP-Item');
+    $('#FP-3').attr('class', 'FP-Item');
     $(this).addClass('FP-Active');
-    $('#FP-5').attr('class','FP-Item');
-    $('#FP-6').attr('class','FP-Item');
+    $('#FP-5').attr('class', 'FP-Item');
+    $('#FP-6').attr('class', 'FP-Item');
     $('#FP-6').css('border-radius', '0 0 5px 5px');
     $('#sign').css('font-family', 'Lobster-Regular');
     $('#FP-Placeholder').css('color', 'black');
+    $('#FP-Placeholder').html('Lobster-Regular <img src = "img/expandArrow.svg" alt = "" style = "float: right; margin-right: 15px;">');
     $('.fontPicker').css('border', '1px solid #1EA896');
   })
   $('#FP-5').click(function() {
-    $('#FP-1').attr('class','FP-Item');
+    $('#FP-1').attr('class', 'FP-Item');
     $('#FP-1').css('border-radius', '5px 5px 0 0');
-    $('#FP-2').attr('class','FP-Item');
-    $('#FP-3').attr('class','FP-Item');
-    $('#FP-4').attr('class','FP-Item');
+    $('#FP-2').attr('class', 'FP-Item');
+    $('#FP-3').attr('class', 'FP-Item');
+    $('#FP-4').attr('class', 'FP-Item');
     $(this).addClass('FP-Active');
-    $('#FP-6').attr('class','FP-Item');
+    $('#FP-6').attr('class', 'FP-Item');
     $('#FP-6').css('border-radius', '0 0 5px 5px');
     $('#sign').css('font-family', 'MonAmour');
     $('#FP-Placeholder').css('color', 'black');
+    $('#FP-Placeholder').html('MonAmour <img src = "img/expandArrow.svg" alt = "" style = "float: right; margin-right: 15px;">');
     $('.fontPicker').css('border', '1px solid #1EA896');
   })
   $('#FP-6').click(function() {
-    $('#FP-1').attr('class','FP-Item');
+    $('#FP-1').attr('class', 'FP-Item');
     $('#FP-1').css('border-radius', '5px 5px 0 0');
-    $('#FP-2').attr('class','FP-Item');
-    $('#FP-3').attr('class','FP-Item');
-    $('#FP-4').attr('class','FP-Item');
-    $('#FP-5').attr('class','FP-Item');
+    $('#FP-2').attr('class', 'FP-Item');
+    $('#FP-3').attr('class', 'FP-Item');
+    $('#FP-4').attr('class', 'FP-Item');
+    $('#FP-5').attr('class', 'FP-Item');
     $(this).addClass('FP-Active');
     $(this).css('border-radius', '0 0 3px 3px');
     $('#sign').css('font-family', 'Sladkoeshka');
     $('#FP-Placeholder').css('color', 'black');
+    $('#FP-Placeholder').html('Sladkoeshka <img src = "img/expandArrow.svg" alt = "" style = "float: right; margin-right: 15px;">');
     $('.fontPicker').css('border', '1px solid #1EA896');
   })
   $('body').click(function(evt) {
@@ -102,9 +108,8 @@ $(document).ready(function() {
     $('#fontPicker').css('height', '50px');
     $('#FP-Placeholder').css('z-index', '10');
     $('#FP-Placeholder').css('opacity', '100');
-    $('#FP-1').css('z-index','1');
+    $('#FP-1').css('z-index', '1');
     $('.FP-Item').css('display', 'none')
     $('.FP-Item').css('opacity', '0')
-    $('.FP-Item').css('padding-top', '0px')
   });
 });
