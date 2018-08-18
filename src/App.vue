0d144c27186623ebd3c3bda4e172d6969d00c6a6
ExEr7um <exer7um@gmail.com>
2018-08-18 17:31:51 +0300

    Revert "Фикс для Windows"
    This reverts commit 482e080170e83234b017c8a0341f31919e8ffbbf.

--- src/App.vue
+++ src/App.vue
@@ -34,64 +34,8 @@ export default {
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css?family=Montserrat:300,400,900&amp;subset=cyrillic,cyrillic-ext,latin-ext');
-
 #app {
     font-family: 'Montserrat', sans-serif;
     margin: 0;
-}
-* {
-  -webkit-box-sizing: border-box;
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-  font-family: "Montserrat", sans-serif;
-}
-
-img {
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
-}
-
-h1 {
-  font-size: 58px;
-  font-weight: 900;
-  margin: 0;
-  padding: 0;
-}
-
-h2 {
-  font-size: 36px;
-  font-weight: 900;
-  margin: 0;
-  padding: 0;
-  padding-top: 50px;
-}
-
-h3 {
-  font-size: 22px;
-  font-weight: 900;
-  margin: 0;
-  padding: 0;
-  margin-top: 10px;
-}
-
-h4 {
-  font-size: 18px;
-  font-weight: 400;
-  margin-top: 10px;
-  margin-bottom: 0;
-}
-
-h4:first-child {
-  margin-top: 0;
-}
-
-.caption {
-  font-size: 12px;
-  text-transform: uppercase;
-  color: #828282;
 }
 </style>
