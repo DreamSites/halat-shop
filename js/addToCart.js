e12f9fefb8cfb5122f42f9d035ce4eb1e461affe
ExEr7um <exer7um@gmail.com>
2019-01-04 03:22:15 +0300

    Всё очистил

--- js/addToCart.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $('.addToCart').click(function() {
-    var $input = $('#countcart');
-    $input.val(parseInt($input.val()) + 1);
-    $input.change();
-    return false;
-  });
-});
